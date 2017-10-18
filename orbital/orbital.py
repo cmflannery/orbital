bb0b0045e3f9d039766664c9baf401cd83bfe614
cflannery <cmflannery@ucsd.edu>
2017-10-17 22:15:24 -0700

    adding properties

--- orbital/orbital.py
+++ orbital/orbital.py
@@ -2,13 +2,52 @@
 
 """ orbital: a orbit and trajectory design and analysis tool """
 
-from astropy.time import TimeEpochDate
-
+from
 class orbit-base():
     """ orbit-base is the base class used for orbital design """
-    def __init__(self, *, pos, vel, epoch, **kwargs):
-        print(pos, vel, epoch)
-        print(kwargs)
+    def __init__(self, **kwargs):
+        elements = kwargs
+
+    def epoch():
+        doc = "The epoch property."
+        def fget(self):
+            return self._epoch
+        def fset(self, value):
+            self._epoch = value
+        def fdel(self):
+            del self._epoch
+        return locals()
+
+    def epoch():
+        doc = "The epoch property."
+        def fget(self):
+            return self._epoch
+        def fset(self, value):
+            self._epoch = value
+        def fdel(self):
+            del self._epoch
+        return locals()
+
+    def epoch():
+        doc = "The epoch property."
+        def fget(self):
+            return self._epoch
+        def fset(self, value):
+            self._epoch = value
+        def fdel(self):
+            del self._epoch
+        return locals()
+
+    def epoch():
+        doc = "The epoch property."
+        def fget(self):
+            return self._epoch
+        def fset(self, value):
+            self._epoch = value
+        def fdel(self):
+            del self._epoch
+        return locals()
+epoch = property(**epoch())
 
 if __name__ == '__main__':
     sat = orbit(pos=[0, 0, 0], vel=[0, 0, 0], epoch=TimeEpochDate.value)
